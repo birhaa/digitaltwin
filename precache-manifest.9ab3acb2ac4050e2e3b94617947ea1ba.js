dafebd0cf42b407db46bb96444e7a8316e5c375c
Birgitte Haavardsholm <birgitte.haavardsholm@gmail.com>
2019-09-18 19:43:19 +0200

    Updates

---
+++ precache-manifest.9ab3acb2ac4050e2e3b94617947ea1ba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04d5ca0f6801b5c8031aaddba29f838b",
+    "url": "/digitaltwin/index.html"
+  },
+  {
+    "revision": "e3a0138c33ead010133a",
+    "url": "/digitaltwin/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "502568fae27f046b1641",
+    "url": "/digitaltwin/static/js/2.8efdccc4.chunk.js"
+  },
+  {
+    "revision": "e3a0138c33ead010133a",
+    "url": "/digitaltwin/static/js/main.c4b2a94e.chunk.js"
+  },
+  {
+    "revision": "258c96a563f80ed882c2",
+    "url": "/digitaltwin/static/js/runtime~main.b0091175.js"
+  }
+]);

40b11b100b741d31390c0438cb5e07dc6aeb71ec
Johanne Håøy Horn <johanne.hh@gmail.com>
2019-09-24 18:00:11 +0200

    Move images and run prettier on files

--- src/MainFolder.js
+++ src/MainFolder.js
@@ -1,71 +1,69 @@
-
-
 import * as dat from 'dat.gui';
 
-
 dat.GUI.prototype.removeFolder = function(name) {
-  var folder = this.__folders[name];
-  if (!folder) {
-    return;
-  }
-  folder.close();
-  this.__ul.removeChild(folder.domElement.parentNode);
-  delete this.__folders[name];
-  this.onResize();
-}
+    var folder = this.__folders[name];
+    if (!folder) {
+        return;
+    }
+    folder.close();
+    this.__ul.removeChild(folder.domElement.parentNode);
+    delete this.__folders[name];
+    this.onResize();
+};
 
 var params = {
-  nCols : 8,
-  nColsMask : 8,
-  useOffset : false,
-  mirror : false,
-  mirrorHalf : false,
-  mirrorOffset : false,
-  mask : true,
-  animationSpeed : 0
+    nCols: 8,
+    nColsMask: 8,
+    useOffset: false,
+    mirror: false,
+    mirrorHalf: false,
+    mirrorOffset: false,
+    mask: true,
+    animationSpeed: 0
+};
 
+class MainFolder {
+    constructor(gui, resetFunction) {
+        this.mainFolder = gui.addFolder('MAIN');
+    }
+
+    getFolder() {
+        return this.mainFolder;
+    }
+
+    initProps(resetFunction) {
+        this.mainFolder.add(params, 'nCols', 1, 100, 1).onChange(() => {
+            resetFunction(this.mainFolder, params);
+        });
+        this.mainFolder.add(params, 'nColsMask', 1, 100, 1).onChange(() => {
+            resetFunction(this.mainFolder, params);
+        });
+        this.mainFolder.add(params, 'mirror').onChange(() => {
+            resetFunction(this.mainFolder, params);
+        });
+        resetFunction(this.mainFolder, params);
+        this.mainFolder.add(params, 'mirrorHalf').onChange(() => {
+            resetFunction(this.mainFolder, params);
+        });
+        resetFunction(this.mainFolder, params);
+        this.mainFolder.add(params, 'useOffset').onChange(() => {
+            resetFunction(this.mainFolder, params);
+        });
+        resetFunction(this.mainFolder, params);
+        this.mainFolder.add(params, 'mirrorOffset').onChange(() => {
+            resetFunction(this.mainFolder, params);
+        });
+        this.mainFolder.add(params, 'mask').onChange(() => {
+            resetFunction(this.mainFolder, params);
+        });
+        this.mainFolder.add(params, 'animationSpeed', 0, 5, 0.1);
+
+        this.mainFolder.open();
+    }
+
+    getSettings() {
+        return params;
+    }
 }
 
-class MainFolder{
-
-  constructor(gui, resetFunction){
-      this.mainFolder = gui.addFolder( 'MAIN' );
-  }
-
-  getFolder(){
-    return this.mainFolder;
-  }
-
-  initProps(resetFunction){
-    this.mainFolder.add(params, 'nCols', 1, 100, 1)
-    .onChange( () => {resetFunction(this.mainFolder, params)} );
-    this.mainFolder  .add(params, 'nColsMask', 1, 100, 1)
-    .onChange( () => {resetFunction(this.mainFolder, params)} );
-    this.mainFolder.add(params, 'mirror' )
-    .onChange( () => {resetFunction(this.mainFolder, params)} );
-    resetFunction(this.mainFolder, params)
-    this.mainFolder.add(params, 'mirrorHalf' )
-    .onChange( () => {resetFunction(this.mainFolder, params)} );
-    resetFunction(this.mainFolder, params)
-    this.mainFolder.add(params, 'useOffset' )
-    .onChange( () => {resetFunction(this.mainFolder, params)} );
-    resetFunction(this.mainFolder, params)
-    this.mainFolder.add(params, 'mirrorOffset' )
-    .onChange( () => {resetFunction(this.mainFolder, params)} );
-    this.mainFolder.add(params, 'mask' )
-    .onChange( () => {resetFunction(this.mainFolder, params)} );
-    this.mainFolder.add(params, 'animationSpeed', 0, 5,0.1 )
-
-    this.mainFolder.open();
-  }
-
-  getSettings(){
-    return params;
-  }
-
-
-}
-
-export {
-  MainFolder,
-}
+export { MainFolder };
--- src/MaterialFolder.js
+++ src/MaterialFolder.js
@@ -1,161 +1,240 @@
-
-
 import * as dat from 'dat.gui';
 
-
 dat.GUI.prototype.removeFolder = function(name) {
-  var folder = this.__folders[name];
-  if (!folder) {
-    return;
-  }
-  folder.close();
-  this.__ul.removeChild(folder.domElement.parentNode);
-  delete this.__folders[name];
-  this.onResize();
+    var folder = this.__folders[name];
+    if (!folder) {
+        return;
+    }
+    folder.close();
+    this.__ul.removeChild(folder.domElement.parentNode);
+    delete this.__folders[name];
+    this.onResize();
+};
+
+class MaterialFolder {
+    constructor(gui, name) {
+        this.gui = gui;
+        this.name = name;
+
+        this.settings = {
+            useOffset: true,
+            stripes: 0.0,
+            metalness: 1.0,
+            roughness: 0.4,
+            ambientIntensity: 0.2,
+            aoMapIntensity: 1.0,
+            envMapIntensity: 1.0,
+            displacementScale: 0.1,
+            normalScale: 0.0,
+            color1: 0xffffff,
+            playWave: false,
+            waveSpeed: 1.0,
+            waveSizeX: 0.0,
+            waveSizeY: 0.0,
+            waveFrequency: 1.0,
+            rainbowX: 0.0,
+            rainbowY: 0.0,
+            rainbowZ: 0.0,
+            blurRadius: 0.0,
+            blurResX: 0.0,
+            blurResY: 0.0
+        };
+
+        this.params = {
+            color1: 0xffffff,
+            rainbowX: 0.0,
+            rainbowY: 0.0,
+            rainbowZ: 0.0,
+            blurRadius: 0.0,
+            blurResX: 0.0,
+            blurResY: 0.0
+        };
+    }
+
+    initShaderProps(material, materialShader) {
+        this.gui.removeFolder(this.name);
+        this.mainFolder = this.gui.addFolder(this.name);
+
+        this.mainFolder
+            .add(this.settings, 'stripes', 0.0, 10.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.size.value = this.settings.stripes)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'useOffset')
+            .onChange(
+                () =>
+                    (materialShader.uniforms.useOffset.value = this.settings.useOffset)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'metalness')
+            .min(0)
+            .max(1)
+            .onChange(function(value) {
+                material.metalness = value;
+            });
+
+        this.mainFolder
+            .add(this.settings, 'roughness')
+            .min(0)
+            .max(1)
+            .onChange(function(value) {
+                material.roughness = value;
+            });
+
+        // this.mainFolder.add( this.settings, "ambientIntensity" ).min( 0 ).max( 1 ).onChange( function ( value ) {
+        //   ambientLight.intensity = value;
+        // } );
+        this.mainFolder
+            .add(this.settings, 'displacementScale')
+            .min(0)
+            .max(3.0)
+            .onChange(function(value) {
+                material.displacementScale = value;
+            });
+
+        this.mainFolder
+            .add(this.settings, 'normalScale')
+            .min(-1)
+            .max(1)
+            .onChange(function(value) {
+                material.normalScale.set(1, -1).multiplyScalar(value);
+            });
+
+        this.mainFolder
+            .addColor(this.settings, 'color1')
+            .onChange(() => material.color.setHex(this.settings.color1));
+
+        this.mainFolder
+            .add(this.settings, 'playWave')
+            .onChange(
+                () =>
+                    (materialShader.uniforms.playWave.value = this.settings.playWave)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'waveSpeed', 0.0, 5.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.waveSpeed.value = this.settings.waveSpeed)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'waveFrequency', 0.0, 100.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.waveFrequency.value = this.settings.waveFrequency)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'waveSizeX', 0.0, 0.1)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.waveSize.value.x = this.settings.waveSizeX)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'waveSizeY', 0.0, 0.1)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.waveSize.value.y = this.settings.waveSizeY)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'rainbowX', 0.0, 1.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.rainbow1Dir.value.x = this.settings.rainbowX)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'rainbowY', 0.0, 1.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.rainbow1Dir.value.y = this.settings.rainbowY)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'rainbowZ', 0.0, 1.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.rainbow1Dir.value.z = this.settings.rainbowZ)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'blurRadius', 0.0, 5.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.blurRadius1.value = this.settings.blurRadius)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'blurResX', 0.0, 1000.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.blurRes1.value.x = this.settings.blurResX)
+            );
+
+        this.mainFolder
+            .add(this.settings, 'blurResY', 0.0, 1000.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.blurRes1.value.y = this.settings.blurResY)
+            );
+
+        this.folder1 = this.mainFolder.addFolder('STRIPE');
+
+        this.folder1
+            .add(this.params, 'rainbowX', 0.0, 1.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.rainbow2Dir.value.x = this.params.rainbowX)
+            );
+
+        this.folder1
+            .add(this.params, 'rainbowY', 0.0, 1.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.rainbow2Dir.value.y = this.params.rainbowY)
+            );
+
+        this.folder1
+            .add(this.params, 'rainbowZ', 0.0, 1.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.rainbow2Dir.value.z = this.params.rainbowZ)
+            );
+
+        this.folder1
+            .add(this.params, 'blurRadius', 0.0, 5.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.blurRadius2.value = this.params.blurRadius)
+            );
+
+        this.folder1
+            .add(this.params, 'blurResX', 0.0, 1000.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.blurRes2.value.x = this.params.blurResX)
+            );
+
+        this.folder1
+            .add(this.params, 'blurResY', 0.0, 1000.0)
+            .onChange(
+                () =>
+                    (materialShader.uniforms.blurRes2.value.y = this.params.blurResY)
+            );
+
+        this.mainFolder.open();
+    }
+
+    getSettings() {
+        return this.settings;
+    }
 }
 
-
-
-class MaterialFolder{
-
-  constructor(gui, name){
-    this.gui = gui;
-    this.name = name;
-
-    this.settings = {
-        useOffset : true,
-        stripes: 0.0,
-      	metalness: 1.0,
-      	roughness: 0.4,
-      	ambientIntensity: 0.2,
-      	aoMapIntensity: 1.0,
-      	envMapIntensity: 1.0,
-      	displacementScale: 0.1,
-      	normalScale: 0.0,
-        color1: 0xffffff,
-        playWave : false,
-        waveSpeed : 1.0,
-        waveSizeX : 0.00,
-        waveSizeY : 0.00,
-        waveFrequency : 1.0,
-        rainbowX : 0.0,
-        rainbowY : 0.0,
-        rainbowZ : 0.0,
-        blurRadius : 0.0,
-        blurResX : 0.0,
-        blurResY : 0.0
-    };
-
-    this.params = {
-        color1: 0xffffff,
-        rainbowX : 0.0,
-        rainbowY : 0.0,
-        rainbowZ : 0.0,
-        blurRadius : 0.0,
-        blurResX : 0.0,
-        blurResY : 0.0
-    };
-
-  }
-
-  initShaderProps(material, materialShader){
-
-    this.gui.removeFolder(this.name);
-    this.mainFolder = this.gui.addFolder( this.name );
-
-    this.mainFolder.add(this.settings, 'stripes', 0.0, 10.0)
-    .onChange( () => materialShader.uniforms.size.value = this.settings.stripes );
-
-    this.mainFolder.add(this.settings, 'useOffset')
-    .onChange( () =>  materialShader.uniforms.useOffset.value = this.settings.useOffset );
-
-    this.mainFolder.add( this.settings, "metalness" ).min( 0 ).max( 1 ).onChange( function ( value ) {
-      material.metalness = value;
-    } );
-
-    this.mainFolder.add( this.settings, "roughness" ).min( 0 ).max( 1 ).onChange( function ( value ) {
-      material.roughness = value;
-    } );
-
-    // this.mainFolder.add( this.settings, "ambientIntensity" ).min( 0 ).max( 1 ).onChange( function ( value ) {
-    //   ambientLight.intensity = value;
-    // } );
-    this.mainFolder.add( this.settings, "displacementScale" ).min( 0 ).max( 3.0 ).onChange( function ( value ) {
-      material.displacementScale = value;
-    } );
-
-    this.mainFolder.add( this.settings, "normalScale" ).min( - 1 ).max( 1 ).onChange( function ( value ) {
-      material.normalScale.set( 1, - 1 ).multiplyScalar( value );
-    } );
-
-    this.mainFolder.addColor(this.settings, 'color1' )
-    .onChange( () => material.color.setHex(this.settings.color1) );
-
-    this.mainFolder.add(this.settings, 'playWave')
-    .onChange( () =>  materialShader.uniforms.playWave.value = this.settings.playWave );
-
-    this.mainFolder.add(this.settings, 'waveSpeed', 0.0,5.0)
-    .onChange( () =>  materialShader.uniforms.waveSpeed.value = this.settings.waveSpeed );
-
-    this.mainFolder.add(this.settings, 'waveFrequency', 0.0,100.0)
-    .onChange( () =>  materialShader.uniforms.waveFrequency.value = this.settings.waveFrequency );
-
-    this.mainFolder.add(this.settings, 'waveSizeX', 0.0,0.1)
-    .onChange( () =>  materialShader.uniforms.waveSize.value.x = this.settings.waveSizeX );
-
-    this.mainFolder.add(this.settings, 'waveSizeY', 0.0,0.1)
-    .onChange( () =>  materialShader.uniforms.waveSize.value.y = this.settings.waveSizeY );
-
-    this.mainFolder.add(this.settings, 'rainbowX', 0.0, 1.0)
-    .onChange( () =>  materialShader.uniforms.rainbow1Dir.value.x = this.settings.rainbowX );
-
-    this.mainFolder.add(this.settings, 'rainbowY', 0.0, 1.0)
-    .onChange( () =>  materialShader.uniforms.rainbow1Dir.value.y = this.settings.rainbowY );
-
-    this.mainFolder.add(this.settings, 'rainbowZ', 0.0, 1.0)
-    .onChange( () =>  materialShader.uniforms.rainbow1Dir.value.z = this.settings.rainbowZ );
-
-    this.mainFolder.add(this.settings, "blurRadius", 0.0, 5.0)
-    .onChange( () =>  materialShader.uniforms.blurRadius1.value = this.settings.blurRadius );
-
-    this.mainFolder.add(this.settings, "blurResX", 0.0, 1000.0)
-    .onChange( () =>  materialShader.uniforms.blurRes1.value.x = this.settings.blurResX );
-
-    this.mainFolder.add(this.settings, "blurResY", 0.0, 1000.0)
-    .onChange( () =>  materialShader.uniforms.blurRes1.value.y = this.settings.blurResY );
-
-    this.folder1 = this.mainFolder.addFolder( 'STRIPE' );
-
-    this.folder1.add(this.params, 'rainbowX', 0.0, 1.0)
-    .onChange( () =>  materialShader.uniforms.rainbow2Dir.value.x = this.params.rainbowX );
-
-    this.folder1.add(this.params, 'rainbowY', 0.0, 1.0)
-    .onChange( () =>  materialShader.uniforms.rainbow2Dir.value.y = this.params.rainbowY );
-
-    this.folder1.add(this.params, 'rainbowZ', 0.0, 1.0)
-    .onChange( () =>  materialShader.uniforms.rainbow2Dir.value.z = this.params.rainbowZ );
-
-    this.folder1.add(this.params, "blurRadius", 0.0, 5.0)
-    .onChange( () =>  materialShader.uniforms.blurRadius2.value = this.params.blurRadius );
-
-    this.folder1.add(this.params, "blurResX", 0.0, 1000.0)
-    .onChange( () =>  materialShader.uniforms.blurRes2.value.x = this.params.blurResX );
-
-    this.folder1.add(this.params, "blurResY", 0.0, 1000.0)
-    .onChange( () =>  materialShader.uniforms.blurRes2.value.y = this.params.blurResY );
-
-
-    this.mainFolder.open();
-
-  }
-
-  getSettings(){
-    return this.settings;
-  }
-
-
-}
-
-export {
-  MaterialFolder
-}
+export { MaterialFolder };
